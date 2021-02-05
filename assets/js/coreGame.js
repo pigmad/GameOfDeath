dc7d1633efb71969bef045adbbfd225d12bc1e3b
Dorian <lastennet.dorian@live.fr>
2021-02-05 13:39:14 +0100

    base jeu
    génération plateau jeu, quelques cellules et rendu graphique

--- assets/js/coreGame.js
+++ assets/js/coreGame.js
@@ -1,35 +1,101 @@
-class species {
+class Species {
     constructor(actionsArray, alive, color) {
         this.actionsArray = actionsArray;
         this.alive = alive;
         this.color = color;
     }
-    
-    get actionsArray(){
-        return this.actionsArray;
+
+    static getSituation(selectedSpecies, x, y, board) {
+        var neigh = [];
+        neigh.push(encodeSitutation(selectedSpecies, board[(x - 1) % board.xMax][y]));
+        neigh.push(encodeSitutation(selectedSpecies, board[(x + 1) % board.xMax][y]));
+        neigh.push(encodeSitutation(selectedSpecies, board[x][(y - 1) % board.yMax]));
+        neigh.push(encodeSitutation(selectedSpecies, board[x][(y + 1) % board.yMax]));
+
+        return 1 + neigh[0] + 3 * neigh[1] + 9 * neigh[2] + 27 * neigh[3];
     }
-    
-    get alive(){
-        return this.alive;
-    }
-    
-    get color(){
-        return this.color;
+
+    static encodeSituation(selectedSpecies, cellValue) {
+        if (cellValue === -1) {
+            return 0;
+        } else {
+            if (selectedSpecies !== cellValue) {
+                return 2;
+            } else {
+                return 1;
+            }
+        }
     }
 }
 
-class world{
-    constructor(speciesArray, maxSpecies, pMut) {
+class World {
+    constructor(xMax, yMax, board, cycle, speciesArray, maxSpecies, pMut) {
+        this.xMax = xMax;
+        this.yMax = yMax;
+        this.board = board;
+        this.cycle = cycle;
         this.speciesArray = speciesArray;
-        this.maxSpecies = maxSpecies; 
-        this.pMut = pMut; 
-        var cvs = document.getElementById("canvas");
-        this.xMax = cvs.width;
-        this.yMax = cvs.height;
-        this.board = new Array(this.xmax%20);
-        for (var i = 0; i < this.xmax%20; i++) {
-            this.board[i] = new Array(3);
-        }
+        this.maxSpecies = maxSpecies;
+        this.pMut = pMut;
     }
-    
 }
+
+//nombre maximum d'espèces dans le monde
+SPECIESNUMBER = 3;
+//nombre de situtation perÃ§ue par une cellule en fonction de son voisinage 
+//4 cellules voisines aux cardinaux (N,S,E,O) * 3 valeur de cellules possible (0 = rien, 1 = mÃªme espèce, 2 = espèce différente)
+// = 3^4 = 81)
+MAXSITUATIONS = 81;
+//nombre maximum d'actions qu'une cellule peut faire, les codes associés sont définis ci-dessous
+ACTIONSNUMBER = 4;
+
+//ACTION CODES
+NORTH = 1 //créer une cellule au nord
+SOUTH = 2 //créer une cellule au nord
+WEST = 3 //créer une cellule au nord
+EAST = 4 //créer une cellule au nord
+
+function initSpecies() {
+    species = [];
+    for (var i = 0; i < SPECIESNUMBER; i++) {
+        //génération du génotype de l'espèce
+        actionArray = [];
+        for (var j = 0; j < MAXSITUATIONS; j++) {
+            actionArray.push(1 + Math.floor(Math.random() * ACTIONSNUMBER));
+        }
+        //génération de la couleur de la cellule
+        color = [1 + Math.floor(Math.random() * 255), 1 + Math.floor(Math.random() * 255), 1 + Math.floor(Math.random() * 255)];
+        var s = new Species(actionArray, true, color);
+        species.push(s);
+    }
+    return species;
+}
+
+function initWorld() {
+    var xMax = cvs.width / STEP;
+    var yMax = cvs.height / STEP;
+    var board = [];
+    var cycle = 0;
+    var pMut = 0.5;
+    var pCreate = 0.05;
+    var species = initSpecies();
+    for (var x = 0; x < xMax; x++) {
+        var line = [];
+        for (var y = 0; y < yMax; y++) {
+            if (Math.random() < pCreate) {
+                //la valeur d'une cellule 
+                line.push(Math.floor(Math.random() * SPECIESNUMBER));
+            } else {
+                line.push(-1);
+            }
+        }
+        board.push(line);
+    }
+    var world = new World(xMax, yMax, board, cycle, species, SPECIESNUMBER, pMut);
+    drawWorld(world);
+    window.world = world;
+
+}
+
+initWorld();
+console.log(world);
--- assets/js/rendering.js
+++ assets/js/rendering.js
@@ -1,27 +1,49 @@
 function init() {
-    window.onresize = init;
-    var cvs = document.getElementById("canvas"),
-    ctx = cvs.getContext("2d");
-    cvs.width = cvs.offsetWidth - (cvs.offsetWidth % 20);
-    cvs.height = cvs.offsetHeight - (cvs.offsetHeight % 20);
-    drawGrid(ctx, cvs.width, cvs.height, 20);
+    window.cvs = document.getElementById("canvas");
+    window.ctx = cvs.getContext("2d");
+    window.STEP = 20;
+    //window.onresize = resizeCanvas(cvs);
+    resizeCanvas();
+    drawGrid();
 }
 
-function drawGrid(ctx, w, h, step) {
+function resizeCanvas() {
+    cvs.width = cvs.offsetWidth - (cvs.offsetWidth % STEP);
+    cvs.height = cvs.offsetHeight - (cvs.offsetHeight % STEP);
+}
+
+function drawGrid() {
     ctx.beginPath();
-    for (var x = step; x <= w - step; x += step) {
+    var w = cvs.width;
+    var h = cvs.height;
+    for (var x = STEP; x <= w - STEP; x += STEP) {
         ctx.moveTo(x, 0);
         ctx.lineTo(x, h);
     }
     ctx.strokeStyle = 'rgb(0,0,0)';
     ctx.lineWidth = 1;
-    for (var y = step; y <= h - step; y += step) {
+    for (var y = STEP; y <= h - STEP; y += STEP) {
         ctx.moveTo(0, y);
         ctx.lineTo(w, y);
     }
     ctx.stroke();
 }
 
-window.onload = init;
+function drawWorld(world) {
+    drawGrid();
+    for (var i = 0; i < world.xMax; i++) {
+        for (var j = 0; j < world.yMax; j++) {
+            var cellValue = world.board[i][j];
+            if (cellValue !== -1) {
+                var color = world.speciesArray[cellValue].color;
+                ctx.fillStyle = 'rgb(' + color[0] + ',' + color[1] + ',' + color[2] + ')';
+                ctx.fillRect(i * STEP + 1, j * STEP + 1, STEP - 2, STEP - 2);
+            }
+        }
+    }
+}
+
+//initialisation de la grille de jeu au chargement de la page
+init();
 
 
